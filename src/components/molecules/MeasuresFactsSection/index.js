76132d09393c62c023cab9d8b068b7fc82b2dcf9
Miguel Morales <mmorales@idesoluciones.com>
2017-08-24 16:03:25 -0500

    Se agrega responsive a los componentes MeasuresFactsSection y OrganismSection

--- src/components/molecules/MeasuresFactsSection/index.js
+++ src/components/molecules/MeasuresFactsSection/index.js
@@ -1,28 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { Grid, Row, Col } from 'react-flexbox-grid'
+import { Row, Col } from 'react-flexbox-grid'
+import theme from '../../themes/default'
 
-const Wrapper = styled.div `
+const Wrapper = styled.div`
   .title-two {
-    font-size: 30px;
-    padding-left: 20px;
-    color: #4B5353;
-    margin-top: 40px;
+    font-size: 24px;
+    color: ${theme.palette.text[0]};
+    margin-top: 60px;
   }
   .accent-title {
-    margin-top: 10px;
-    border-top: 2px solid #ff7847;
+    margin-top: 20px;
+    border-top: 2px solid ${theme.palette.basescale[6]};
   }
 
   .more-details {
-    color: #4B5353;
-    font-size: 20px;
-    line-height: 45px;
-    font-weight: 200;
-    padding: 30px 0px;
+    color: ${theme.palette.text[0]};
+    font-size: ${theme.fonts.subTitleTwo.font};
+    line-height: 28px;
+    font-weight: ${theme.fonts.subTitleTwo.weight};
+    padding: 20px;
     .details-title{
       font-weight: 600;
+      margin-top: 1px;
+      margin-bottom: 1px;
     }
   }
 `
@@ -49,30 +51,30 @@ class MeasuresFactsSection extends React.Component {
   render() {
     return (
       <Wrapper>
-        <Col className="title-two" md={12}>Medidas o hechos</Col>
-        <Col className="accent-title" md={11} />
-        <Grid fluid>
+        <Col className="title-two" xs={12} sm={12} md={12} lg={12}>Medidas o hechos</Col>
+        <Col className="accent-title" xs={11} sm={11} md={11} lg={11} />
+        <Col xs={12} sm={12} md={12} lg={12}>
           <Row className="more-details">
-            {this.state.measurementOrFact.measurementID && <Col md={6} className="details-title">Identificador</Col>}
-            {this.state.measurementOrFact.measurementID && <Col md={6}>{this.state.measurementOrFact.measurementID}</Col>}
-            {this.state.measurementOrFact.measurementType && <Col md={6} className="details-title">Tipo</Col>}
-            {this.state.measurementOrFact.measurementType && <Col md={6}>{this.state.measurementOrFact.measurementType}</Col>}
-            {this.state.measurementOrFact.measurementValue && <Col md={6} className="details-title">Valor</Col>}
-            {this.state.measurementOrFact.measurementValue && <Col md={6}>{this.state.measurementOrFact.measurementValue}</Col>}
-            {this.state.measurementOrFact.measurementAccuracy && <Col md={6} className="details-title">Precisión</Col>}
-            {this.state.measurementOrFact.measurementAccuracy && <Col md={6}>{this.state.measurementOrFact.measurementAccuracy}</Col>}
-            {this.state.measurementOrFact.measurementUnit && <Col md={6} className="details-title">Unidad de medida</Col>}
-            {this.state.measurementOrFact.measurementUnit && <Col md={6}>{this.state.measurementOrFact.measurementUnit}</Col>}
-            {this.state.measurementOrFact.measurementDeterminedBy && <Col md={6} className="details-title">Determinado por</Col>}
-            {this.state.measurementOrFact.measurementDeterminedBy && <Col md={6}>{this.state.measurementOrFact.measurementDeterminedBy}</Col>}
-            {this.state.measurementOrFact.measurementDeterminedDate && <Col md={6} className="details-title">Fecha</Col>}
-            {this.state.measurementOrFact.measurementDeterminedDate && <Col md={6}>{this.state.measurementOrFact.measurementDeterminedDate}</Col>}
-            {this.state.measurementOrFact.measurementMethod && <Col md={6} className="details-title">Método</Col>}
-            {this.state.measurementOrFact.measurementMethod && <Col md={6}>{this.state.measurementOrFact.measurementMethod}</Col>}
-            {this.state.measurementOrFact.measurementRemarks && <Col md={6} className="details-title">Comentarios</Col>}
-            {this.state.measurementOrFact.measurementRemarks && <Col md={6}>{this.state.measurementOrFact.measurementRemarks}</Col>}
+            {this.state.measurementOrFact.measurementID && <Col xs={12} sm={6} md={6} lg={6} className="details-title">Identificador</Col>}
+            {this.state.measurementOrFact.measurementID && <Col xs={12} sm={6} md={6} lg={6}>{this.state.measurementOrFact.measurementID}</Col>}
+            {this.state.measurementOrFact.measurementType && <Col xs={12} sm={6} md={6} lg={6} className="details-title">Tipo</Col>}
+            {this.state.measurementOrFact.measurementType && <Col xs={12} sm={6} md={6} lg={6}>{this.state.measurementOrFact.measurementType}</Col>}
+            {this.state.measurementOrFact.measurementValue && <Col xs={12} sm={6} md={6} lg={6} className="details-title">Valor</Col>}
+            {this.state.measurementOrFact.measurementValue && <Col xs={12} sm={6} md={6} lg={6}>{this.state.measurementOrFact.measurementValue}</Col>}
+            {this.state.measurementOrFact.measurementAccuracy && <Col xs={12} sm={6} md={6} lg={6} className="details-title">Precisión</Col>}
+            {this.state.measurementOrFact.measurementAccuracy && <Col xs={12} sm={6} md={6} lg={6}>{this.state.measurementOrFact.measurementAccuracy}</Col>}
+            {this.state.measurementOrFact.measurementUnit && <Col xs={12} sm={6} md={6} lg={6} className="details-title">Unidad de medida</Col>}
+            {this.state.measurementOrFact.measurementUnit && <Col xs={12} sm={6} md={6} lg={6}>{this.state.measurementOrFact.measurementUnit}</Col>}
+            {this.state.measurementOrFact.measurementDeterminedBy && <Col xs={12} sm={6} md={6} lg={6} className="details-title">Determinado por</Col>}
+            {this.state.measurementOrFact.measurementDeterminedBy && <Col xs={12} sm={6} md={6} lg={6}>{this.state.measurementOrFact.measurementDeterminedBy}</Col>}
+            {this.state.measurementOrFact.measurementDeterminedDate && <Col xs={12} sm={6} md={6} lg={6} className="details-title">Fecha</Col>}
+            {this.state.measurementOrFact.measurementDeterminedDate && <Col xs={12} sm={6} md={6} lg={6}>{this.state.measurementOrFact.measurementDeterminedDate}</Col>}
+            {this.state.measurementOrFact.measurementMethod && <Col xs={12} sm={6} md={6} lg={6} className="details-title">Método</Col>}
+            {this.state.measurementOrFact.measurementMethod && <Col xs={12} sm={6} md={6} lg={6}>{this.state.measurementOrFact.measurementMethod}</Col>}
+            {this.state.measurementOrFact.measurementRemarks && <Col xs={12} sm={6} md={6} lg={6} className="details-title">Comentarios</Col>}
+            {this.state.measurementOrFact.measurementRemarks && <Col xs={12} sm={6} md={6} lg={6}>{this.state.measurementOrFact.measurementRemarks}</Col>}
           </Row>
-        </Grid>
+        </Col>
       </Wrapper>
     )
   }
