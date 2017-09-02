7d64e0f7082990edfec441b443286fd6dddf0b2c
Miguel Morales <mimoralesga@gmail.com>
2017-09-02 14:40:37 -0500

    Se agrega maquetado inicial del item de taxonomia del menu lateral

--- src/components/molecules/TaxonomySearchItem/index.js
+++ src/components/molecules/TaxonomySearchItem/index.js
@@ -6,8 +6,14 @@ import MenuItem from 'material-ui/MenuItem'
 import AutoComplete from 'material-ui/AutoComplete'
 import { Grid, Row, Col } from 'react-flexbox-grid'
 import Checkbox from 'material-ui/Checkbox'
+import theme from '../../themes/default'
 
 const Wrapper = styled.div`
+  .active-filter{
+    padding-top: 10px;
+    padding-bottom: 10px;
+    background-color: #F3F8F8;
+  }
 `
 
 const colors = [
@@ -32,19 +38,23 @@ export default class TaxonomySearchItem extends Component {
     super(props)
     this.state = {
       value: 1,
+      open: true,
     }
+
+    this.activeFilter = this.activeFilter.bind(this)
   }
 
   handleChange = (event, index, value) => this.setState({ value })
+  activeFilter() { this.setState({ open: !this.state.open }) }
 
   render() {
     return (
-      <Wrapper>
+      <Wrapper onClick={this.activeFilter}>
         <FileSearchItem title="Taxonomía">
           <Grid fluid className="without-padding divider">
             <Row middle="xs">
               <Col xs={12} sm={12} md={12} lg={12}>
-                <Checkbox label="Tyrannus melacholicus" />
+                <Checkbox checked label="Tyrannus melacholicus" iconStyle={{ fill: theme.palette.basescale[6] }} />
               </Col>
             </Row>
           </Grid>
@@ -70,7 +80,34 @@ export default class TaxonomySearchItem extends Component {
             underlineFocusStyle={{ borderColor: '#FF7847' }}
             hintStyle={{ color: '#838787' }}
           />
+          <Grid fluid className="without-padding">
+            <Row between="xs" middle="xs">
+              <Col xs={6} sm={6} md={6} lg={6}>
+                <Checkbox label="..." />
+              </Col>
+              <Col xs={6} sm={6} md={3} lg={3}>55.814</Col>
+            </Row>
+            <Row between="xs" middle="xs">
+              <Col xs={6} sm={6} md={6} lg={6}>
+                <Checkbox label="..." />
+              </Col>
+              <Col xs={6} sm={6} md={3} lg={3}>57.326</Col>
+            </Row>
+            <Row between="xs" middle="xs">
+              <Col xs={6} sm={6} md={6} lg={6}>
+                <Checkbox label="..." />
+              </Col>
+              <Col xs={6} sm={6} md={3} lg={3}>58.614</Col>
+            </Row>
+          </Grid>
         </FileSearchItem>
+        {this.state.open && <Grid fluid className="without-padding" style={{ paddingTop: 10, paddingBottom: 10, background: '#F3F8F8' }}>
+          <Row middle="xs" center="xs">
+            <Col xs={12} sm={12} md={12} lg={12}>
+              <Checkbox checked label="Tyrannus melacholicus" iconStyle={{ fill: theme.palette.basescale[6] }} />
+            </Col>
+          </Row>
+        </Grid>}
       </Wrapper>
     )
   }
