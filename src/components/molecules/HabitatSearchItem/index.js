3d64c40f4d3c6bb28d8e1e8f449128a5f3485a69
Miguel Morales <mimoralesga@gmail.com>
2017-09-05 11:48:29 -0500

    Se agrega prototipo a el item del habitat

--- src/components/molecules/HabitatSearchItem/index.js
+++ src/components/molecules/HabitatSearchItem/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import { FileSearchItem } from 'components'
-import SelectField from 'material-ui/SelectField'
-import MenuItem from 'material-ui/MenuItem'
+import Checkbox from 'material-ui/Checkbox'
+import { Grid, Row, Col } from 'react-flexbox-grid'
 import AutoComplete from 'material-ui/AutoComplete'
+import Divider from 'material-ui/Divider'
+import theme from '../../themes/default'
 
 const Wrapper = styled.div`
 `
@@ -30,29 +32,26 @@ export default class HabitatSearchItem extends Component {
     super(props)
     this.state = {
       value: 1,
+      open: true,
     }
+    this.activeFilter = this.activeFilter.bind(this)
   }
 
   handleChange = (event, index, value) => this.setState({ value })
+  activeFilter() { this.setState({ open: !this.state.open }) }
 
   render() {
     return (
       <Wrapper>
-        <FileSearchItem title="Hábitat">
-          <SelectField
-            value={this.state.value}
-            onChange={this.handleChange}
-            labelStyle={{ color: '#838787' }}
-            underlineStyle={{ borderColor: '#FF7847' }}
-            iconStyle={{ fill: '#838787' }}
-            underlineFocusStyle={{ borderColor: '#FF7847' }}
-          >
-            <MenuItem value={1} primaryText="Páramo" />
-            <MenuItem value={2} primaryText="Every Night" />
-            <MenuItem value={3} primaryText="Weeknights" />
-            <MenuItem value={4} primaryText="Weekends" />
-            <MenuItem value={5} primaryText="Weekly" />
-          </SelectField>
+        <FileSearchItem title="Hábitat" onExpandChange={this.activeFilter}>
+          <Grid fluid className="without-padding divider">
+            <Row middle="xs">
+              <Col xs={12} sm={12} md={12} lg={12}>
+                <Checkbox checked label="Páramo" iconStyle={{ fill: theme.palette.basescale[6] }} />
+              </Col>
+            </Row>
+          </Grid>
+          <Divider style={{ marginTop: 10, marginBottom: 10 }} />
           <AutoComplete
             hintText="Escriba el nombre del hábitat"
             dataSource={colors}
@@ -62,6 +61,13 @@ export default class HabitatSearchItem extends Component {
             hintStyle={{ color: '#838787' }}
           />
         </FileSearchItem>
+        {this.state.open && <Grid fluid style={{ paddingTop: 10, paddingBottom: 10, background: '#F3F8F8' }}>
+          <Row middle="xs">
+            <Col xs={12} sm={12} md={12} lg={12}>
+              <Checkbox checked label="Páramo" iconStyle={{ fill: theme.palette.basescale[6] }} />
+            </Col>
+          </Row>
+        </Grid>}
       </Wrapper>
     )
   }
