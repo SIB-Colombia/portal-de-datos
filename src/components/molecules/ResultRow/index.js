2fdc98c53eaf739b06561f7571207653f6fd11c9
Jorge Gonzalez <jag2kn@gmail.com>
2017-10-26 12:08:49 -0500

    Se ajustan campos llamados del servicio GBIF

--- src/components/molecules/ResultRow/index.js
+++ src/components/molecules/ResultRow/index.js
@@ -16,18 +16,36 @@ class ResultRow extends React.Component {
   redirect(url) {
     window.location.href = url
   }
+  
+  ubicacion(r){
+    var t = ""
+    if (r.decimalLatitude){
+      if (r.decimalLatitude>0)
+        t += r.decimalLatitude.toFixed(2)+" N"
+      else
+        t += (-r.decimalLatitude).toFixed(2)+" S"
+    }
+    if (r.decimalLongitude){
+      t += ", "
+      if (r.decimalLongitude>0)
+        t += r.decimalLongitude.toFixed(2)+" E"
+      else
+        t += (-r.decimalLongitude).toFixed(2)+" O"
+    }
+    return t
+  }
 
   render() {
     console.log("Id: ")
     console.log(this.props.registro.key)
     return (
       <TableRow hoverable onClick={() => this.redirect(`/occurrence/${this.props.registro.key}`)} className="hover">
-        <TableRowColumn>{this.props.registro.species ? this.props.registro.species : ''}</TableRowColumn>
+        <TableRowColumn>{this.props.registro.scientificName ? this.props.registro.scientificName : ''}</TableRowColumn>
         <TableRowColumn>{this.props.registro.country ? this.props.registro.country : ''}</TableRowColumn>
         <TableRowColumn>{this.props.registro.stateProvince ? this.props.registro.stateProvince : ''}</TableRowColumn>
-        <TableRowColumn>{this.props.registro.location ? `${(this.props.registro.decimalLatitude).toFixed(2)}, ${(this.props.registro.decimalLongitude).toFixed(2)}` : ''}</TableRowColumn>
+        <TableRowColumn>{this.ubicacion(this.props.registro)}</TableRowColumn>
         <TableRowColumn>{this.props.registro.basisOfRecord ? this.props.registro.basisOfRecord : ''}</TableRowColumn>
-        <TableRowColumn>{this.props.registro.year ? this.props.registro.year : ''}</TableRowColumn>
+        <TableRowColumn>{this.props.registro.eventDate ? this.props.registro.eventDate.substring(0, 10) : ''}</TableRowColumn>
         <TableRowColumn>{this.props.registro.institutionCode ? this.props.registro.institutionCode : ''}</TableRowColumn>
         <TableRowColumn>{this.props.registro.identifier ? this.props.registro.identifier : ''}</TableRowColumn>
         <TableRowColumn>{this.props.registro.taxonRank ? this.props.registro.taxonRank : ''}</TableRowColumn>
